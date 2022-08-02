f3c945fe29d0f236fd793d67fe767c45a35ebbc0
vaslim <mislav.jancic@protonmail.com>
2022-08-02 22:44:24 +0200

    Ardutooth module added, real time location tracking added

--- Android/app/src/main/java/com/vaslim/autopilot/MainActivity.java
+++ Android/app/src/main/java/com/vaslim/autopilot/MainActivity.java
@@ -2,9 +2,16 @@ package com.vaslim.autopilot;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -12,10 +19,14 @@ import com.vaslim.autopilot.fragments.AutopilotFragment;
 import com.vaslim.autopilot.fragments.ManualFragment;
 import com.vaslim.autopilot.fragments.SettingsFragment;
 
+import io.github.giuseppebrb.ardutooth.Ardutooth;
+
+
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
 
+    Ardutooth ardutooth;
     AutopilotFragment autopilotFragment = new AutopilotFragment();
     ManualFragment manualFragment = new ManualFragment();
     SettingsFragment settingsFragment = new SettingsFragment();
@@ -24,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},99);
+        ardutooth = Ardutooth.getInstance(this);
+        ardutooth.setConnection();
+
 
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,autopilotFragment).commit();
@@ -33,7 +48,12 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()){
                     case R.id.menu_autopilot: {
-                        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,autopilotFragment).commit();
+                        if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,autopilotFragment).commit();
+                        }else{
+                            Toast.makeText(MainActivity.this, "No location permission", Toast.LENGTH_SHORT).show();
+                        }
+
                         return true;
                     }
                     case R.id.menu_manual: {
@@ -49,5 +69,8 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
+
+
     }
+
 }
--- Android/app/src/main/java/com/vaslim/autopilot/fragments/AutopilotFragment.java
+++ Android/app/src/main/java/com/vaslim/autopilot/fragments/AutopilotFragment.java
@@ -1,66 +1,112 @@
 package com.vaslim.autopilot.fragments;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.vaslim.autopilot.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AutopilotFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class AutopilotFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public static final int DEFAULT_UPDATE_INTERVAL = 100;
+    public static final int FAST_UPDATE_INTERVAL = 50;
+    FusedLocationProviderClient fusedLocationProviderClient;
+    LocationRequest locationRequest;
+    LocationCallback locationCallback;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView tvGPSBearing, tvGPSAccuracy;
 
     public AutopilotFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AutopilotFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AutopilotFragment newInstance(String param1, String param2) {
-        AutopilotFragment fragment = new AutopilotFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(DEFAULT_UPDATE_INTERVAL);
+        locationRequest.setFastestInterval(FAST_UPDATE_INTERVAL);
+        locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                updateViews(locationResult.getLastLocation());
+            }
+        };
+
+        updateGPS();
+        locationUpdates();
+
+    }
+
+    private void locationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
         }
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_autopilot, container, false);
+        View view = inflater.inflate(R.layout.fragment_autopilot, container, false);
+
+        return view;
+    }
+
+
+    private void updateGPS(){
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.getActivity());
+        if(ActivityCompat.checkSelfPermission(this.getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(this.getActivity(), new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    updateViews(location);
+                }
+            });
+        }
+        else{
+            this.getActivity().requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},99);
+        }
+    }
+
+    private void updateViews(Location location){
+        tvGPSBearing = this.getActivity().findViewById(R.id.tv_gps_bearing);
+        tvGPSAccuracy = this.getActivity().findViewById(R.id.tv_gps_accuracy);
+        //float bearing = location.getBearing();
+        //System.out.println("ACCURACY"+location.getAccuracy());
+        tvGPSBearing.setText(String.valueOf(location.getBearing()));
+        tvGPSAccuracy.setText(String.valueOf(location.getAccuracy()) + " "+location.getLatitude()+";"+location.getLongitude());
     }
 }
