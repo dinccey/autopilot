6084f7dab38a2bf6365f2d6249d4ab1c1e382ed0
vaslim <mislav.jancic@protonmail.com>
2022-08-15 21:09:23 +0200

    Rework turning algorithm

--- Android/app/src/main/java/com/vaslim/autopilot/AutopilotThread.java
+++ Android/app/src/main/java/com/vaslim/autopilot/AutopilotThread.java
@@ -4,7 +4,7 @@ import com.vaslim.autopilot.fragments.AutopilotFragment;
 
 public class AutopilotThread extends Thread{
 
-    public static final double CONTROLLER_ROTATION_LENGTH_TIME_SECONDS = 0.5;
+    public static final double NORMALIZER = 30;
     private static final double PAUSE_BEFORE_SPIN = 100;
     private static final double CYCLE_SLEEP = 3000;
     public volatile boolean running = true;
@@ -34,49 +34,35 @@ public class AutopilotThread extends Thread{
                 Turn turn = calculateTurn(targetBearing,currentBearing);
                 System.out.println("TURN: "+turn.direction+", "+turn.offsetDegrees);
 
-                //number of turns to send to arduino
-                double rotationCommandsCount =  ((turn.offsetDegrees/100*sensitivity) / CONTROLLER_ROTATION_LENGTH_TIME_SECONDS);
-                int rotateAbstractTime= (int) rotationCommandsCount;
-                //cannot turn less than once
-                System.out.println("ROTATION COUNT "+rotationCommandsCount);
-                if(rotationCommandsCount<1 && rotationCommandsCount>0.1) rotateAbstractTime = 1;
-                else if(turn.offsetDegrees < 0.7) rotateAbstractTime = 0;
-                //send to arduino
-                System.out.println("ROTATE "+rotateAbstractTime);
-                sendToController(turn,rotateAbstractTime);
+                double lengthOfTurn = (turn.offsetDegrees * sensitivity) / NORMALIZER;
+                sendToController(turn,lengthOfTurn);
+
                 sleepMilliseconds(CYCLE_SLEEP);
-                //System.out.println("isConnected: "+MainActivity.ardutooth.isConnected());
 
             }
         }
     }
 
-    private void sendToController(Turn turn, int rotationCommandsCount) {
-        if (rotationCommandsCount == 0) return;
-        char turnTo = 'N';
+    private void sendToController(Turn turn, double lengthOfTurn) {
+        char turnTo;
         //make the turn;
         if(turn.direction == Turn.Direction.RIGHT){
             turnTo = CHAR_TURN_RIGHT;
         }else{
             turnTo = CHAR_TURN_LEFT;
         }
-        MainActivity.ardutooth.sendChar(turnTo);
-        double sleepTime = CONTROLLER_ROTATION_LENGTH_TIME_SECONDS * 1000 * rotationCommandsCount;
-        System.out.println("SLEEP TIME: "+sleepTime/1000 + "s");
-        sleepMilliseconds(sleepTime);
-        if(rotationCommandsCount == 1) {
-            MainActivity.ardutooth.sendChar(CHAR_TURN_STOP);
-            sleepMilliseconds(PAUSE_BEFORE_SPIN);
-            return;
-        };
-        //return rudder to (almost) previous position
-        sleepMilliseconds(PAUSE_BEFORE_SPIN);
-        if(turnTo == CHAR_TURN_LEFT) turnTo = CHAR_TURN_RIGHT;
-        else if(turnTo == CHAR_TURN_RIGHT) turnTo = CHAR_TURN_LEFT;
+        System.out.println("SLEEP TIME: "+lengthOfTurn/1000 + "s");
 
         MainActivity.ardutooth.sendChar(turnTo);
-        sleepTime = CONTROLLER_ROTATION_LENGTH_TIME_SECONDS*1000*(rotationCommandsCount-1);
-        sleepMilliseconds(sleepTime);
+        sleepMilliseconds(lengthOfTurn);
+        MainActivity.ardutooth.sendChar(CHAR_TURN_STOP);
+        //return rudder to (almost) previous position
+
+        if(turnTo == CHAR_TURN_LEFT) turnTo = CHAR_TURN_RIGHT; //TODO put this in Turn class
+        else if(turnTo == CHAR_TURN_RIGHT) turnTo = CHAR_TURN_LEFT;
+        sleepMilliseconds(lengthOfTurn);
+        MainActivity.ardutooth.sendChar(turnTo);
+        sleepMilliseconds(lengthOfTurn-(lengthOfTurn*0.1));
 
         MainActivity.ardutooth.sendChar(CHAR_TURN_STOP);
         sleepMilliseconds(PAUSE_BEFORE_SPIN);
